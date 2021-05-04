a5f96adc3de090424b74b9ed863ca2fe359233d5
btyrrell15 <51797754+btyrrell15@users.noreply.github.com>
2021-05-04 11:57:32 -0600

    Update charge.js

--- paypage/js/charge.js
+++ paypage/js/charge.js
@@ -1,6 +1,6 @@
 // Set your publishable key: remember to change this to your live publishable key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
-var stripe = Stripe('pk_test_QWawzoGYjYXB9LtpFqHw5aCD00I2sAu8TY');
+var stripe = Stripe('');
 var elements = stripe.elements();
 
 // Custom styling can be passed to options when creating an Element.
